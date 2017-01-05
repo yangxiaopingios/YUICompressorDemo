7c727499baf7ed621ed48efc6415b343cf5b9a43
yangxiaoping <yangxiaoping@ihealthlabs.com.cn>
2017-01-05 12:10:20 +0800

    YUI Compressor Demo

---
+++ CompressorProject/ih_script/ih_compressor.js
@@ -0,0 +1,46 @@
+/**
+ * Created by yangyang on 17/1/5.
+ */
+    
+var compressor = require('yuicompressor');
+var fs = require( "fs" );
+var path = require('path');
+
+compressor.compress(path.resolve('./bootstrap.css'), {
+    // defaults to 'utf8' 被压缩文件编码
+    charset: 'utf8',
+    // defaults to 'js'    被压缩文件类型
+    type: 'js',
+    // defaults to 'false'
+    //当为false 被压缩文件自动缩短 JavaScript 文件中的变量名。
+    nomunge: true,
+    //多长内容换行
+    'line-break': 80
+}, async function(err, data, extra) {
+    //err  当不为null 压缩出现错误
+    console.log(err);
+    //data  压缩完的字符串
+    if(!err){
+        let result = await ih_writeFile('bootstrap.js', data);
+        if(result){
+            console.log('压缩成功啦');
+        }
+    }
+    //extra 警告消息
+    console.log(extra);
+});
+
+function ih_writeFile(fileName, data){
+    return new Promise(function (resolve, reject) {
+        var fileNameArr = fileName.split('.')
+        fs.writeFile(path.resolve('./' + fileNameArr[0] + '.min.' + fileNameArr[1]), data, function(err){
+            if (err){
+                console.log(err);
+                reject (false);
+            }
+            else {
+                resolve (true);
+            }
+        });
+    });
+}
